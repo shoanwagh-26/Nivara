0bc55086151e2e238b0a60522a9e87c5d8d907e2
Shoan Wagh <shonwagh2003@gmail.com>
2026-08-31 19:33:09 +0530

    Deploy dashboard with Amplify

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -4,9 +4,11 @@ rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute/default
 mkdir -p ./.amplify-hosting/static
+mkdir -p ./.amplify-hosting/static/dashboard
 
 cp -r ./backend/* ./.amplify-hosting/compute/default/
 
 cp -r ./frontend/build/* ./.amplify-hosting/static/
+cp -r ./dashboard/build/* ./.amplify-hosting/static/dashboard/
 
 cp ./deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
